b763cc6c82f92ce595633b8f20fee18067c1a9cb
Jimin Pyo <dkwlrhdwn20@gmail.com>
2026-06-16 10:07:53 +0900

    fix(user-test): assertFalse import 누락 추가

--- modules/user/user-application/src/test/java/com/fandrops/user/application/listener/AgencyApprovalEmailListenerTest.java
+++ modules/user/user-application/src/test/java/com/fandrops/user/application/listener/AgencyApprovalEmailListenerTest.java
@@ -14,6 +14,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
